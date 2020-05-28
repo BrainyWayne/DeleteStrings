ecb474cd192fc79757d3aaed3019272c9820999a
Francis Eshun <eshunfrancisboateng@gmail.com>
2020-05-28 15:12:18 -0500

    Final Working Solution

--- src/com/francis/Main.java
+++ src/com/francis/Main.java
@@ -1,8 +1,39 @@
 package com.francis;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	
+        String s;
+        String remove;
+
+        Scanner scanner = new Scanner(System.in);
+        printOut("Enter the string s\n");
+        s = scanner.nextLine();
+        printOut("Enter the characters to remove\n");
+        remove = scanner.nextLine();
+
+        printOut(deleteChars(s,remove));
+
+
+
+    }
+
+    static String deleteChars(String s, String remove){
+        char[] charsToRemove;
+        charsToRemove = remove.toCharArray();
+
+
+        for(int i = 0; i < charsToRemove.length; i++){
+           s = s.replaceAll(String.valueOf(charsToRemove[i]), "");
+
+        }
+
+        return s;
+    }
+
+
+    static void printOut(String string){
+        System.out.print(string);
     }
 }
